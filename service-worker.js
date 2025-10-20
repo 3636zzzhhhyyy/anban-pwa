7902a7bd9e177f46fcd09ac04bc549cc01cb817a
3636zzzhhhyyy <zhy360810@qq.com>
2025-10-20 15:00:38 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'anban-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/product.html',
+  '/solution.html',
+  '/team.html',
+  '/contact.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// 安装Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 拦截请求并使用缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 缓存命中则返回缓存，否则请求网络
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// 清理旧缓存
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
